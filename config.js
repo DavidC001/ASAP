d5828a55239501816c496aa0520f13c7b0163989
DavidC001 <davidecavicchini@hotmail.com>
2024-03-29 17:32:26 +0100

    https -> http

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
 
-    host: "https://cuwu.ddns.net:8082", //"http://localhost:8080", // https://deliveroojs.onrender.com"
+    host: "http://cuwu.ddns.net:8082", //"http://localhost:8080", // https://deliveroojs.onrender.com"
 
     token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjQzNjBhOTE4OTU4IiwibmFtZSI6ImxvbCIsImlhdCI6MTcxMTQ1Mzc2NH0.opEO5mYymDmxgOIA0lS2kCgxEmc4BYqtYQr22xzxDWo'
 
