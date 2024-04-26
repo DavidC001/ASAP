0712c06e6981c6523b1e5377546d713a2cc65e5a
DavidC001 <davidecavicchini@hotmail.com>
2024-04-26 17:04:53 +0200

    doc: commented and documented intentions code

--- intentions/intentions.js
+++ intentions/intentions.js
@@ -2,19 +2,39 @@ import { map } from '../beliefs/map/map.js';
 import { me } from '../beliefs/beliefs.js';
 import { parcels } from '../beliefs/parcels/parcels.js';
 import { EventEmitter } from 'events';
+import { DeliverooApi } from '@unitn-asa/deliveroo-js-client';
 
 const MAX_RETRIES = 1;
-const stopEmitter = new EventEmitter();
+const stopEmitter = new EventEmitter(); //TODO: make a diffierent emitter for each intention
 
+/**
+ * @class Intention
+ * 
+ * @property {{x:number,y:number}} goal - The goal of the intention
+ * @property {string} pickUp - The id of the parcel to pick up, null if the intention is not to pick up a parcel
+ * @property {boolean} deliver - True if the intention is to deliver a parcel
+ * @property {string} type - The type of the intention
+ * @property {Array<{move:string}>} plan - The plan to reach the goal
+ * @property {boolean} stop - True if the intention has to stop
+ * @property {boolean} reached - True if the goal has been reached
+ */
 class Intention {
     goal;
     type;
     pickUp;
     deliver;
     plan;
-    stop;
+    stop; //TODO: use events to stop intentions
     reached;
 
+    /**
+     * Creates an instance of Intention.
+     * 
+     * @param {{x:number,y:number}} goal - The goal of the intention 
+     * @param {string} pickUp - The id of the parcel to pick up, null if the intention is not to pick up a parcel
+     * @param {boolean} deliver - True if the intention is to deliver a parcel
+     * @param {string} type - The type of the intention
+     */
     constructor(goal, pickUp=false, deliver=false, type) {
         this.goal = goal;
         this.pickUp = pickUp;
@@ -25,25 +45,42 @@ class Intention {
         this.reached = false;
     }
 
-    async executeInt(client) { //generate plan
+    /**
+     * Plans and executes the intention, tries to be resilient to failed moves and can be stopped
+     * 
+     * @param {DeliverooApi} client
+     */
+    async executeInt(client) {
         this.reached = false;
-        if(this.deliver) {
-            this.goal = map.map[me.x][me.y].closest_delivery;
-            console.log('delivering to', this.goal)
-            this.plan = map.BFS(me, this.goal)
-        } else if(!this.deliver && !this.pickUp){
-            this.goal = {
-                x: Math.floor(Math.random() * (map.width-me.config.PARCELS_OBSERVATION_DISTANCE+1)), 
-                y: Math.floor(Math.random() * (map.height-me.config.PARCELS_OBSERVATION_DISTANCE+1))
-            };
-            console.log('random goal', this.goal);
-            this.plan = map.BFS(me, this.goal);
-        } else {
-            console.log('picking up', this.goal);
-            this.plan = map.BFS(me, this.goal);
+
+        switch (this.type) {
+            case 'pickup':
+                //if the intention is to pick up a parcel, the goal is the parcel position
+                console.log('picking up', this.goal);
+                this.plan = map.BFS(me, this.goal);
+                break;
+            case 'deliver':
+                //if the intention is to deliver a parcel, the goal is the closest delivery point (TODO: in the future use a specific planner for this)
+                this.goal = map.map[me.x][me.y].closest_delivery;
+                console.log('delivering to', this.goal)
+                this.plan = map.BFS(me, this.goal)
+                break;
+            case 'explore':
+                //if the intention is explore, the goal is a random point in the map (TODO: also in the future use a specific planner for this)
+                this.goal = {
+                    x: Math.floor(Math.random() * (map.width-me.config.PARCELS_OBSERVATION_DISTANCE+1)), 
+                    y: Math.floor(Math.random() * (map.height-me.config.PARCELS_OBSERVATION_DISTANCE+1))
+                };
+                console.log('explore goal', this.goal);
+                this.plan = map.BFS(me, this.goal);
+                break;
+            default:
+                console.log('Invalid intention type');
         }
+
         console.log('plan', me.x, me.y, this.plan);
 
+        //execute the plan (TODO: make it more resilient to failed moves and put it in the planner)
         let retryCount = 0;
         for (let i = 0; i < this.plan.length; i++) {
             //console.log(this.type,'move', this.plan[i]);
@@ -62,10 +99,16 @@ class Intention {
             }
             if (this.stop) break;
         }
+
+        //execute the intention
         if (this.pickUp && !this.stop) {
             let res = await client.pickup();
             //console.log('pickup', res.length);
-            if (res.length>0) carriedParcels.push(map.map[this.goal.x][this.goal.y].parcel.id);
+            //if successful add the parcels to the carried parcels
+            for (let p of res) { //TODO: check if this works
+                carriedParcels.push(p.id);
+            }
+            console.log('carried parcels', carriedParcels);
         } 
         if (this.deliver && !this.stop) {
             //console.log('deliver');
@@ -73,22 +116,31 @@ class Intention {
             //empty carried parcels
             carriedParcels.length = 0;
         }
+
         if (this.stop) {
+            //if the intention has to stop send a signal
             console.log('stopped intention', this.type);
             this.stop = false;
             stopEmitter.emit('stoppedIntention');
         }else{
+            //if the goal has been reached set the reached flag
             this.reached = true;
         }
     }
 
+    /**
+     * Computes the utility of the intention
+     * 
+     * @returns {number} the utility of the intention
+     */
     utility() {
-        if (!this.deliver && !this.pickUp) return 1; //random intention
         //TODO: consider other agents going after them
 
         let utility = 0;
         let numParcels = carriedParcels.length;
         let toRemove = []
+
+        //compute the score of the carried parcels
         let score = carriedParcels.reduce((acc, id) => {
             if (parcels.has(id)) {
                 return acc + parcels.get(id).score
@@ -97,30 +149,46 @@ class Intention {
                 return acc;
             }
         }, 0);
-
+        //remove parcels that are no longer carried
         for (let id of toRemove) {
             carriedParcels.splice(carriedParcels.indexOf(id), 1);
         }
 
-
-        if (this.pickUp) {
-            //if an agent is on the same position as the parcel return -1
-            if (map.map[this.goal.x][this.goal.y].agent !== null
-                || map.map[this.goal.x][this.goal.y].parcel === null) {
-                utility = -1;
-            } else {
-                score += parcels.get(this.pickUp).score;
-                let steps = map.BFS(me, this.goal).length; //TODO: if too slow use manhattan distance
-                utility = score - steps * (numParcels) / me.moves_per_parcel_decay - map.map[this.goal.x][this.goal.y].heuristic  * (numParcels) / me.moves_per_parcel_decay
-            }
-        } else if (this.deliver) {
-            let steps = map.map[this.goal.x][this.goal.y].heuristic;
-            utility = score - steps * (numParcels) / me.moves_per_parcel_decay
+        let steps=0;
+        switch (this.type) {
+            case 'pickup':
+                //TODO: if carried parcel over limit return -1
+                if (map.map[this.goal.x][this.goal.y].agent !== null
+                    || map.map[this.goal.x][this.goal.y].parcel === null) {
+                    //if an agent is on the same position as the parcel return -1
+                    score = -1;
+                } else {
+                    //otherwise compute the utility as the score of the parcel minus the steps to reach it
+                    score += parcels.get(this.pickUp).score;
+                    //TODO: if too slow use manhattan distance
+                    steps = map.BFS(me, this.goal).length 
+                            + map.map[this.goal.x][this.goal.y].heuristic; 
+                }
+                break;
+            case 'deliver':
+                //use the heuristic to the closest delivery point
+                steps = map.map[this.goal.x][this.goal.y].heuristic;
+                break;
+            case 'explore':
+                score = 1;
+                steps = 0;
+                break;
+            default:
+                console.log('Invalid intention type');
         }
-
+                
+        utility = score - steps * (numParcels) / me.moves_per_parcel_decay;
         return utility;
     }
 
+    /**
+     * Stops the intention
+     */
     stopInt() {
         this.stop = true;
         console.log('stopping intention', this.type);
@@ -132,20 +200,38 @@ class Intention {
     }
 }
 
+/**
+ * @class Intentions
+ * 
+ * @property {Array<Intention>} intentions - The list of intentions
+ * @property {Intention} currentIntention - The current intention
+ */
 class Intentions {
     intentions;
     currentIntention = null;
 
+    /**
+     * Creates an instance of Intentions.
+     */
     constructor() {
         this.intentions = [];
     }
 
+    /**
+     * Adds an intention to the list of intentions
+     * @param {Intention} intention 
+     */
     addIntention(intention) {
         this.intentions.push(intention);
     }
 
+    /**
+     * Selects the intention with the highest utility and executes it
+     * @param {DeliverooApi} client
+     */
     selectIntention(client) {
         //console.log('intentions', this.intentions);
+
         //find the intention with the highest utility
         let maxUtility = -Infinity;
         let maxIntention = null;
@@ -160,50 +246,60 @@ class Intentions {
         }
 
         if (this.currentIntention === null) {
+            //if there is no current intention start the one with the highest utility
             console.log("starting intention", maxIntention.type, "to", maxIntention.goal);
             this.currentIntention = maxIntention;
             this.currentIntention.executeInt(client);
         } else if (this.currentIntention.goal !== maxIntention.goal) {
+            //if the goal is different from the current intention switch intention
             console.log('switching intention', maxIntention.type, "to", maxIntention.goal, " from", this.currentIntention.type, "to", this.currentIntention.goal);
-            //if goal is not reached
+
             let oldIntention = this.currentIntention;
             this.currentIntention = maxIntention;
+
+            //wait for the current intention to stop before starting the new one
             stopEmitter.once('stoppedIntention', () => {
                 console.log("starting intention", maxIntention.type);
                 this.currentIntention.executeInt(client);
             });
             oldIntention.stopInt();
-        } else if(this.currentIntention.reached && this.currentIntention.type === 'random') {
+        } else if(this.currentIntention.reached && this.currentIntention.type === 'explore') {
+            //if the current intention is explore and the goal has been reached, continue with the next intention
             console.log('continue intention', maxIntention.type);
             this.currentIntention.executeInt(client);
         }
     }
 
+    /**
+     * Generates the base intentions for the agent (deliver and expolore)
+     */
     generateIntentions() {        
         //add deliver intention
         let goal = {x: 0, y: 0}; 
         let pickUp = false;
         let deliver = true;
         this.addIntention(new Intention(goal, pickUp, deliver, 'deliver'));
-        //random goal intention
+        //explore intention
         pickUp = false;
         deliver = false;
-        this.addIntention(new Intention(goal, pickUp, deliver, 'random'));
+        this.addIntention(new Intention(goal, pickUp, deliver, 'explore'));
     }
 
+    /**
+     * Updates the intentions based on the parcels in the map
+     */
     updateIntentions() {
-        //TODO: remove intentions that are no longer possible and add new intentions
-
         let parcelsIDs = new Map();
 
         //remove intentions whose parcels have been picked up or expired
         for (let intention of this.intentions) {
             //console.log('old intention', intention);
-            if (intention.pickUp) {
+            if (intention.type === 'pickup') {
                 parcelsIDs.set(intention.pickUp, true);
-                if (map.map[intention.goal.x][intention.goal.y].parcel === null) {
-                    this.intentions.splice(this.intentions.indexOf(intention), 1);
-                } else if (map.map[intention.goal.x][intention.goal.y].parcel.carried !== null) {
+                if (map.map[intention.goal.x][intention.goal.y].parcel === null
+                    || map.map[intention.goal.x][intention.goal.y].agent !== null
+                    || parcels.get(intention.pickUp).carried !== null) {
+                    //The parcel has either been picked up or expired
                     this.intentions.splice(this.intentions.indexOf(intention), 1);
                 }
             }
@@ -212,6 +308,7 @@ class Intentions {
         //add intentions for new parcels
         for (let [id, parcel] of parcels) {
             if (!parcelsIDs.has(id) && parcel.carried===null) {
+                //Parcel is not in the list of intentions and is not carried
                 console.log('new parcel at', parcel.position);
                 let goal = parcel.position;
                 let pickUp = id;
@@ -223,12 +320,19 @@ class Intentions {
     }
 }
 
+/** @type {Array<string>} */
 const carriedParcels = [];
+/** @type {Intentions} */
 const intentions = new Intentions();
 
+/**
+ * Registers the intention revision functions
+ * 
+ * @param {DeliverooApi} client
+ */
 function IntentionRevision(client) {
     client.onMap(async () => {
-        //wait 1 second for the map to be updated
+        //wait 0.1 second for the map to be created
         await new Promise(resolve => setTimeout(resolve, 100));
         intentions.generateIntentions();
         setInterval(() => {
