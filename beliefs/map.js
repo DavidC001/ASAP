f4743cad72ddba2c0d1052fcb77ddf8907de306e
DavidC001 <davidecavicchini@hotmail.com>
2024-05-31 17:14:51 +0200

    fix: undefined USE_PDDL in env

--- beliefs/map.js
+++ beliefs/map.js
@@ -536,7 +536,7 @@ let map = null;
 function createMap(mapData) {
     map = new Maps(mapData);
     console.log('Map created');
-    map.precalculateCleanBFSPlans(process.env.USE_PDDL);
+    map.precalculateCleanBFSPlans(process.env.USE_PDDL||false);
     setInterval(() => {
         // timeTaken(updateMap);
         updateMap();
